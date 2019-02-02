a271fbdfb2550e53ee0fee4fd2d11bca5da528e6
EmptyWalletException <1833865798@qq.com>
2019-02-02 20:53:01 +0800

    增加README.md文件

---
+++ README.md
@@ -0,0 +1,2 @@
+## 项目简介
+此项目是PhotoLike项目的管理端的后端,与前端分离,采用了springboot+jpa+mysql,前端采用vue+elementUi+nodejs
--- src/main/java/com/example/demo/manager/repositories/RoleRelateUserRepository.java
+++ src/main/java/com/example/demo/manager/repositories/RoleRelateUserRepository.java
@@ -1,5 +1,4 @@
 package com.example.demo.manager.repositories;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
